64610f87a1b6f1683c720465e401ff246aa45f47
Jerem <=>
2022-04-14 23:40:52 +0200

    Implemented store on progress

--- App/src/app/shared/progress/progress.component.ts
+++ App/src/app/shared/progress/progress.component.ts
@@ -1,50 +1,25 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { EventService } from '../../services/event.service';
+import { Component } from '@angular/core';
 import { ProgressBarMode } from '@angular/material/progress-bar';
-import { takeWhile } from 'rxjs/operators';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { selectIsLoading } from 'src/app/store/app.selectors';
+import { AppState } from 'src/app/store/app.state';
 
 @Component({
     selector: 'app-progress',
     templateUrl: './progress.component.html',
     styleUrls: ['./progress.component.sass']
 })
-export class ProgressComponent implements OnInit, OnDestroy
+export class ProgressComponent
 {
-    public isActive = false;
-    public mode: ProgressBarMode = 'indeterminate';
-    public value = 0;
+    public readonly isLoading$: Observable<boolean>;
 
-    private isAlive = true;
-
-    public constructor(private readonly _eventService: EventService) { }
-
-    public ngOnInit(): void
+    public constructor(
+        private readonly _store: Store<AppState>
+    )
     {
-        this.subscribeEvents();
-    }
-
-    public subscribeEvents(): void
-    {
-        this._eventService.startProgressBarEvent.event
-            .pipe(takeWhile(_ => this.isAlive))
-            .subscribe(container =>
-            {
-                this.isActive = true;
-                this.mode = container.mode;
-                this.value = container.value;
-            });
-        this._eventService.stopProgressBarEvent.event
-            .pipe(takeWhile(_ => this.isActive))
-            .subscribe(() =>
-            {
-                this.isActive = false;
-                this.mode = 'indeterminate';
-                this.value = 0;
-            });
-    }
-
-    public ngOnDestroy(): void
-    {
-        this.isAlive = false;
+        this.isLoading$ = this._store.pipe(
+            select(selectIsLoading)
+        );
     }
 }
