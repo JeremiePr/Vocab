0c6a04b70925a08456ef0ecf46f14b803205fd87
Jerem <jeremie.primas@gmail.com>
2022-11-18 00:50:09 +0100

    Update urls

--- App/src/environments/environment.prod.ts
+++ App/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    baseUrl: "http://127.0.0.1:9081/api/v2"
+    baseUrl: "http://127.0.0.1:9081/api"
 };
--- App/src/environments/environment.ts
+++ App/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    baseUrl: "http://localhost:9081/api/v2"
+    baseUrl: "http://localhost:9081/api"
 };
